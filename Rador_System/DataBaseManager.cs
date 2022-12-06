9d5dd126c5af4d3d032355bc3b0ca51dee38ddf4
KimDongsoon <eutteumiyo@naver.com>
2022-12-06 11:02:28 +0900

    First Commit

---
+++ Rador_System/DataBaseManager.cs
@@ -0,0 +1,64 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+public class DataBaseManager
+{
+    MySqlConnection mySqlConnection;
+
+    public MySqlCommand _sqlCommand = null;
+    public MySqlDataReader _sqlDataReader = null;
+
+    string SQL_IP;
+    string SQL_DB;
+    string SQL_ID;
+    string SQL_PW;
+
+    public MySqlConnection myConnection
+    {
+        get
+        {
+            return mySqlConnection;
+        }
+
+        set
+        {
+            mySqlConnection = value;
+        }
+    }
+
+    public DataBaseManager(string ip, string db, string id, string pw)
+    {
+        SQL_IP = ip;
+        SQL_DB = db;
+        SQL_ID = id;
+        SQL_PW = pw;
+    }
+
+    public bool Connect()
+    {
+        string connect = string.Format($"Server={SQL_IP};Database={SQL_DB};Uid={SQL_ID};Pwd={SQL_PW};allow user variables=true;Connect Timeout=7200");
+
+        myConnection = new MySqlConnection(connect);
+        myConnection.Open();
+        var cmd = new MySqlCommand();
+        cmd.Connection = myConnection;
+
+        // 연결 성공 시 true, 실패 시 false 반환
+        if (myConnection.State == ConnectionState.Open)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public void Close()
+    {
+        myConnection.Close();
+    }
+
+}
+
---
+++ Rador_System/RadorMainForm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace Rador_System
+{
+    public partial class RadorMainForm : Form
+    {
+        static DataBaseManager DBManager;
+
+        // SQL DB연결정보 상수
+        const string SQL_IP = "172.20.8.17";
+        const string SQL_DB = "mir_radors";
+        const string SQL_ID = "wemadeQA";
+        const string SQL_PW = "Wemade!@34";
+
+        public RadorMainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void RadorMainForm_Load(object sender, EventArgs e)
+        {
+            DBManager = new DataBaseManager(SQL_IP, SQL_DB, SQL_ID, SQL_PW);
+            DBManager.Connect();
+
+            RunProgram();
+        }
+
+
+
+        private void RunProgram()
+        {
+            DataTable queryData = new DataTable();
+            queryData = GetQueryList("mirm");
+
+        }
+
+        static DataTable GetQueryList(string project)
+        {
+            DataTable dt = new DataTable();
+            string sqlText = $"SELECT * FROM {project}_querys";
+            DBManager._sqlCommand = new MySqlCommand(sqlText, DBManager.myConnection);
+            var adpt = new MySqlDataAdapter(DBManager._sqlCommand);
+            adpt.Fill(dt);
+
+            return dt;
+        }
+    }
+}
